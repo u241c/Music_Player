00ab21b84ad1546455ee5396d736ed4c2861e11b
u241c <jr4701.fsj@gmail.com>
2022-01-30 11:44:38 -0600

    Half 81. complete

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+//music
+const songs = [
+  {  
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  }, 
+  {  
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },  
+  {  
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Metric/Jacinto Design',
+  }, 
+  {  
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Jacinto Design',
+  },    
+];
 
 // Check if Playing
 let isPlaying = false;
@@ -26,3 +53,14 @@ function pauseSong(){
 
  // Play or Pause Event Listener
  playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+ // Update DOM
+ function loadSong(song) {
+     title.textContent = song.displayName;
+     artist.textContent = song.artist;
+     music.src = `music/${song.name}.mp3`;
+     image.src = `img/${song.name}.jpg`;
+ }
+
+ // On Load - Select First Song
+ loadSong(songs[3]);
