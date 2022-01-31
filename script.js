bf7d823fa368efd42b3bc10f29878967bb3534cb
u241c <jr4701.fsj@gmail.com>
2022-01-31 08:21:58 -0600

    Project done

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
-const progressContainer = document.getElementById('progress-container');
-const progress = document.getElementById('progress');
 const currentTimeEl = document.getElementById('current-time');
 const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -96,35 +96,42 @@ function pauseSong(){
  function updateProgressBar(e) {
      if (isPlaying) {
          const {duration, currentTime } = e.srcElement;
+
   // Update progress bar width
-         const progressPercent = (currentTime / duration) * 100;
-         progress.style.width = `${progressPercent}%`;
-    // Calculate display for duration
-    const durationMinutes = Math.floor(duration / 60);  
-    console.log('minutes', durationMinutes); 
-    let durationSeconds = Math.floor(duration % 60);
-    if (durationSeconds < 10) {
-        durationSeconds = `0${durationSeconds}`;
-    }
-    console.log('seconds', durationSeconds);  
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 
-    // Delay Switching duration Element to avoid NaN
-    if (durationSeconds) {
-    durationEl.textContent = `${durationMinutes}:${durationSeconds}`; 
+  // Calculate display for duration
+  const durationMinutes = Math.floor(duration / 60);
+  let durationSeconds = Math.floor(duration % 60);
+  if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+  }
+  // Delay Switching duration Element to avoid NaN
+  if (durationSeconds) {
+  durationEl.textContent = `${durationMinutes}:${durationSeconds}`; 
 }
-    // Calculate display for current
-    const currentMinutes = Math.floor(currentTime / 60);  
-    console.log('minutes', currentMinutes); 
-    let currentSeconds = Math.floor(currentTime % 60);
-    if (currentSeconds < 10) {
-        currentSeconds = `0${currentSeconds}`;
-    }
-    console.log('seconds', currentSeconds); 
-    currentTimeEl.textContent =  `${currentMinutes}:${currentSeconds}`; 
+// Calculate display for currentTime
+const currentMinutes = Math.floor(currentTime / 60);  
+let currentSeconds = Math.floor(currentTime % 60);
+if (currentSeconds < 10) {
+    currentSeconds = `0${currentSeconds}`;
+}
+currentTimeEl.textContent =  `${currentMinutes}:${currentSeconds}`; 
 } 
- }
+}
 
- // Event Listeners
- prevBtn.addEventListener('click', prevSong);
- nextBtn.addEventListener('click', nextSong);
- music.addEventListener('timeupdate', updateProgressBar);
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const {duration} = music;
+  music.currentTime = (clickX / width) * duration;
+}
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
